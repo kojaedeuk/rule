e53f6c17b801c8270a5450b2c530bf8a0628c491
june <ucessdori@gmail.com>
2024-11-15 20:55:45 +0900

    설정 정보 변경
    테스트

--- test/com/june/rule/RuleTest.java
+++ test/com/june/rule/RuleTest.java
@@ -14,48 +14,78 @@ class RuleTest {
 		/*
 		 * 다건 처리 예
 		 */
-		//1. 대상건 조회
-		//2. Fact에 값 세팅
-		//3. Condition 생성
-		//4. Action 생성
-		//5. Rule 조회
 		final Rule ruleMap = RuleBuilder.when(new ConditionMapHandler()).then(new ActionMaptHandler());
 
 		/*
-		 * 조건에 맞는 룰ID찾기
+		 * 수정율 읽기
 		 */
-		List<HashMap<String, Object>> list = new ArrayList<>();
-		for(int i=0; i<10; i++)
-		{
-			HashMap<String, Object> map_sub = new HashMap<>();
-			map_sub.put("Aaa", 10+i);
-			map_sub.put("Bbb", i+1);
-			map_sub.put("Ccc", i);
-			list.add(map_sub);
-		}
-		HashMap<String, Object> map = new HashMap<>();
-		map.put("Bbb", 12);
-		map.put("condition", "@Aaa > Bbb && @Bbb == 5"); //조건식
-		map.put("formula", "@Aaa *2 + Bbb"); //계산식
-		map.put("res_col", "Ccc"); //반환 컬럼
-		map.put("clf_cd", "02"); //분류코드(01:단건,02:다건)
+		List<HashMap<String, Object>> list = getList();
+
+		/*
+		 * 대상건 조회
+		 */
+		HashMap<String, Object> map = getMap();
+		
+		/*
+		 * Fact 생성
+		 */
 	    final Fact fact = new Fact(list, map);
+	    
+	    /*
+	     * rule 실행
+	     */
 	    HashMap<String, Object> res = ruleMap.run(fact);
+	    
 		System.out.println(">>>>"+res); 
 
 		/*
-		 * 단건 처리 예
+		 * 수수료 예
 		 */
 		final Rule rule = RuleBuilder.when(new ConditionHandler()).then(new ActionHandler());
-		fact.setValue("Aaa", 10);
-		fact.setValue("Bbb", 10);
-		fact.setValue("condition", "Aaa >= Bbb"); //조건식
-		fact.setValue("formula", "Aaa *2 + Bbb"); //계산식
-		fact.setValue("res_col", "Ccc"); //반환 컬럼
+		fact.setValue("pRate", 01);
+		fact.setValue("condition", "1"); //조건식
+		fact.setValue("formula", "pRate * aPremium"); //계산식
+		fact.setValue("res_col", "charge"); //반환 컬럼
 		fact.setValue("clf_cd", "01"); //분류코드(01:단건,02:다건)
 	    res = rule.run(fact);
 	    
 		System.out.println(">>>>"+res.get("Ccc")); 
 
+	}
+	
+	/**
+	 * 대상건 map
+	 * @return
+	 */
+	private HashMap<String, Object> getMap()
+	{
+		HashMap<String, Object> map = new HashMap<>();
+		map.put("premium", 100000); //보험료
+		map.put("pod", "A001"); //상품코드
+		map.put("condition", "@pod == 'A001' && @pPeriod == 12"); //조건식
+		map.put("formula", "@cRate * premium"); //계산식
+		map.put("res_col", "aPremium"); //반환 컬럼
+		map.put("clf_cd", "02"); //분류코드(01:단건,02:다건)
+		
+		return map;
+	}
+	
+	/**
+	 * 수정율 list
+	 * @return 수정율
+	 */
+	private List<HashMap<String, Object>> getList()
+	{
+		List<HashMap<String, Object>> list = new ArrayList<>();
+		for(int i=0; i<20; i++)
+		{
+			HashMap<String, Object> map_sub = new HashMap<>();
+			map_sub.put("pod", "A001"); //상품코드
+			map_sub.put("pPeriod", 1+i); //납입회차
+			map_sub.put("cRate", 0.1+i*0.1); //수정율
+			list.add(map_sub);
+		}
+		
+		return list;
 	}
 }

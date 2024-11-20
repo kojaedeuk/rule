7ca4c1f83178a299e1bd2d52dc95fba66dd5c53d
june <ucessdori@gmail.com>
2024-11-20 15:32:13 +0900

    주석 수정
    주석 수정

--- src/com/june/rule/ConditionCalcHandler.java
+++ src/com/june/rule/ConditionCalcHandler.java
@@ -1,3 +1,11 @@
+/**------------------------------------------------------------------------------
+ * PROJ : JUNE PROJECT
+ * NAME : com.june.rule ConditionCalcHandler.java
+ * DESC : Rule system engine Project
+ * VER  : v2.0
+ * Copyright 2000 JUNE All rights reserved
+ *------------------------------------------------------------------------------
+ */
 package com.june.rule;
 
 import java.math.BigDecimal;
